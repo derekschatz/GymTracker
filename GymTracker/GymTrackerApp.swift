b0cd9c9fc3f3bb9de48a25e59da2e98b30fc8853
derekschatz <derekschatz1@gmail.com>
2026-01-21 22:15:15 -0500

    Initial Commit

---
+++ GymTracker/GymTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GymTrackerApp.swift
+//  GymTracker
+//
+//  Created by Derek Schatz on 1/21/26.
+//
+
+import SwiftUI
+
+@main
+struct GymTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
